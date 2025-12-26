85fdd8e35f3a07dd72623a6937694510304c65eb
Muush <2419108998@qq.com>
2025-12-26 17:20:14 +0800

    finish asynchronous echo demo (exit bugs)

--- basicEchoClient.cpp
+++ basicEchoClient.cpp
@@ -9,18 +9,23 @@ const int MAX_LENGTH = 1024;
 
 int main() {
     try {
+        io_context ioContext;
+        ip::tcp::endpoint serverEP(ip::make_address(SERVER_IP), PORT);
+        ip::tcp::socket socket(ioContext);
+        socket.connect(serverEP);
+        std::cout << "Connect success to " << serverEP.address().to_string() << ":" << serverEP.port() << std::endl;
+
         std::cout << ">> ";
         std::string input;
         std::getline(std::cin, input);
-        io_context ioContext;
-        ip::tcp::endpoint serverEP(ip::make_address(SERVER_IP), PORT);
-        ip::tcp::socket socket(ioContext, serverEP.protocol());
-        socket.connect(serverEP);
-        std::cout << "Connect success to " << serverEP.address().to_string() << ": " << serverEP.port() << std::endl;
-        write(socket, buffer(input));
+        write(socket, buffer(input, MAX_LENGTH));
+        std::cout << "Message sent" << std::endl;
+
         char reply[MAX_LENGTH] = {};
-        read(socket, buffer(reply, input.size()));
-        std::cout << "[Receive]: " << reply << std::endl;
+        boost::system::error_code ec;
+        size_t length = socket.read_some(buffer(reply, MAX_LENGTH), ec);
+        if (ec.failed()) throw boost::system::system_error(ec);
+        std::cout << "[Receive]: " << std::string_view(reply, length) << std::endl;
         socket.close();
     } catch (const boost::system::system_error& e) {
         std::cerr << "[Error]: " << e.what() << std::endl;
--- basicEchoServer.cpp
+++ basicEchoServer.cpp
@@ -8,18 +8,20 @@ const int PORT = 10086;
 const int BACKLOG = 30;
 const int MAX_LENGTH = 1024;
 
-void secsion(ip::tcp::socket* socket) {
+void session(ip::tcp::socket* socket) {
     try {
         while (true) {
             char msg[MAX_LENGTH] = {};
             boost::system::error_code ec;
             size_t length = socket->read_some(buffer(msg, MAX_LENGTH), ec);
             if (ec == error::eof) {
+                std::cout << "connection closed by peer" << std::endl;
                 break;
-            } else if (ec.failed()) {
+            } else if (ec.failed()){
                 throw boost::system::system_error(ec);
             }
-            std::cout << "[From]: " << socket->remote_endpoint().address().to_string() << ": " << socket->remote_endpoint().port() << "\n\t" << msg << std::endl;
+            std::cout << "Message recieved" << std::endl;
+            std::cout << "[From]: " << socket->remote_endpoint().address().to_string() << ":" << socket->remote_endpoint().port() << "\n\t" << msg << std::endl;
             write(*socket, buffer(msg, length));
         }
     } catch (const boost::system::system_error& e) {
@@ -29,19 +31,20 @@ void secsion(ip::tcp::socket* socket) {
 
 int main() {
     std::vector<std::shared_ptr<std::thread>> threads;
+    io_context ioContext;
+    ip::tcp::acceptor acceptor(ioContext, ip::tcp::endpoint(ip::tcp::v4(), PORT));
     try {
-        io_context ioContext;
-        ip::tcp::acceptor acceptor(ioContext, ip::tcp::endpoint(ip::tcp::v4(), PORT));
         std::cout << "Listening ..." << std::endl;
         acceptor.listen(BACKLOG);
         ip::tcp::socket* socket = new ip::tcp::socket(ioContext);
         acceptor.accept(*socket);
-        std::shared_ptr t = std::make_shared<std::thread>(secsion, socket);
+        std::cout << "[Connected]: " << socket->remote_endpoint().address().to_string() << ":" << socket->remote_endpoint().port() << std::endl;
+        std::shared_ptr t = std::make_shared<std::thread>(session, socket);
         threads.emplace_back(t);
     } catch (const boost::system::system_error& e) {
         std::cerr << "[Error]: " << e.what() << std::endl;
     }
-    for (auto t : threads)
+    for (auto& t : threads)
         t->join();
     return 0;
 }
